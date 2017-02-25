e425c49a8d5182edc9bd11f26e155045efda1b65
jamie mcgrory <jamie@jamies-imac.home>
2017-02-25 10:47:01 +0000

    Home Styling

--- src/app/services/port.service.ts
+++ src/app/services/port.service.ts
@@ -1,26 +1,28 @@
+// Imports
 import { Injectable }                                 from '@angular/core';
 import { Http, Response, Headers, RequestOptions }    from '@angular/http';
 import { Port }                                       from '../model/port';
 import { Observable }                                 from 'rxjs/Rx';
 
+// Import rxjs js polyfills
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class PortService {
 
-  // Resolve HTTP using the constructor
+  // Resolve HTTP w/ constructor
   constructor (private http: Http) {}
-  // private instance variable to hold base url
+  // placeholder private instance variable to hold base url
   private portUrl = 'http://localhost:3000/app/api/port.json'; 
 
-  // Fetch all existing comments
+  // Fetch all existing Portfolio pieces
   getPorts() : Observable<Port[]> {
-    // ...using get request
+    // JM: http get request
     return this.http.get(this.portUrl)
-      // ...and calling .json() on the response to return data
+      // JM: call .json() on the response to return data
       .map((res:Response) => res.json())
-      //...errors if any
+      // JM: errors if any
       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
   }
 }
